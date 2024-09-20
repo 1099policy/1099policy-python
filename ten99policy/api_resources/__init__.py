4230e45639eefc8ebe683f556f740520aa24518f
Cemre Fatih Karakulak <cradexco@gmail.com>
2024-09-20 12:50:48 +0300

    add support for job_categories and webhooks

--- ten99policy/api_resources/__init__.py
+++ ten99policy/api_resources/__init__.py
@@ -12,7 +12,11 @@ from ten99policy.api_resources.jobs import Jobs
 from ten99policy.api_resources.policies import Policies
 from ten99policy.api_resources.quotes import Quotes
 from ten99policy.api_resources.invoices import Invoices
-from ten99policy.api_resources.insurance_application_sessions import InsuranceApplicationSessions
+from ten99policy.api_resources.insurance_application_sessions import (
+    InsuranceApplicationSessions,
+)
 from ten99policy.api_resources.events import Events
 from ten99policy.api_resources.assignments import Assignments
 from ten99policy.api_resources.contractor_policies import ContractorPolicies
+from ten99policy.api_resources.webhooks import Webhooks
+from ten99policy.api_resources.job_categories import JobCategories
